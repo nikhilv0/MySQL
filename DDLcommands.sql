77b8866e7bcbecb0f06118b4ad314cefcdbde212
nikhilv0 <nikhilnikki6360@gmail.com>
2025-04-20 21:27:32 +0530

    Uploaded Assignment

---
+++ DDLcommands.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE Ecommerce;
+CREATE DATABASE Showroom;
+CREATE DATABASE Workstation;
+CREATE DATABASE Brands;
+
+use Ecommerce;
+use Showroom;
+CREATE TABLE buy(id int,shop varchar(10),sell varchar(10),purchase varchar(10));
+SELECT * FROM buy;
+
+CREATE TABLE del(id int,remov varchar(10),drops varchar(10),purchase varchar(10));
+SELECT * FROM buy;
