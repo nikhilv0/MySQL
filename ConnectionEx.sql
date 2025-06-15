a6be6a76771682d91fba0cfadddfc81fdface38b
nikhilv0 <nikhilnikki6360@gmail.com>
2025-06-15 15:23:15 +0530

    uploaded Assignment

--- ConnectionEx.sql
+++ ConnectionEx.sql
@@ -1,1 +1,6 @@
+SELECT * FROM Connection.Birth_certificate;
+SELECT * FROM Connection.death_Certificate;
+SELECT * FROM Connection.Driving_License;
 SELECT * FROM Connection.job_app;
+SELECT * FROM Connection.Marriage_Certificate;
+SELECT * FROM Connection.Passport;
